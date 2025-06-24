65af094e2915be22ebc7c2f67508aa9e075c492a
Akt99 <arnabkumartripathy@gmail.com>
2025-06-24 19:53:44 +0530

    Initial commit: fullstack taskify project

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const todoRouter = require("./routes/todo");
+const userRouter = require("./routes/user");
+//const { connectToDatabase } = require("./db");
+const db = require("./db");
+console.log("What am I importing from db/index.js?", db);
+
+
+const { connectToDatabase } = db;
+
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/todo", todoRouter);
+app.use("/user", userRouter);
+
+// Establish a connection to the MongoDB database
+connectToDatabase().then(() => {
+    const PORT = process.env.PORT || 3000; 
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
